7f9c6b3d49892cae84f0abdd60025befb99593c6
Stephen Glass <stephen@stephen.glass>
2024-05-31 00:28:26 -0400

    tests

---
+++ plugins/http-client-proxy-backend/src/service/proxy.test.ts
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2022 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { loggerToWinstonLogger } from '@backstage/backend-common';
+import {
+  ConfigSources,
+  MutableConfigSource,
+  StaticConfigSource,
+} from '@backstage/config-loader';
+import express from 'express';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import request from 'supertest';
+import {
+  mockServices,
+  setupRequestMockHandlers,
+} from '@backstage/backend-test-utils';
+import { createProxyAgent } from './proxy';
+import {
+  setGlobalDispatcher,
+  getGlobalDispatcher,
+  Dispatcher,
+  ProxyAgent,
+  Agent,
+  MockAgent,
+  fetch as undiciFetch,
+} from 'undici';
+import { default as nodeFetch } from 'node-fetch';
+
+// https://github.com/jest-community/eslint-plugin-jest/blob/v27.9.0/docs/rules/no-conditional-expect.md
+class NoErrorThrownError extends Error {}
+
+const getError = async <TError>(call: () => unknown): Promise<TError> => {
+  try {
+    await call();
+
+    throw new NoErrorThrownError();
+  } catch (error: unknown) {
+    return error as TError;
+  }
+};
+
+describe('Requests are routed via proxy', () => {
+  beforeEach(() => {
+    process.env.BACKSTAGE_HTTP_PROXY = undefined;
+    process.env.BACKSTAGE_HTTPS_PROXY = undefined;
+    process.env.BACKSTAGE_NO_PROXY = undefined;
+  });
+
+  it('should be able to proxy using native fetch', async () => {
+    const config = await ConfigSources.toConfig(
+      StaticConfigSource.create({
+        data: {
+          proxy: {
+            http_proxy: 'http://localhost:3000',
+            https_proxy: 'http://localhost:3000',
+          },
+        },
+      }),
+    );
+
+    await createProxyAgent({
+      config,
+      logger: loggerToWinstonLogger(mockServices.logger.mock()),
+    });
+
+    const error: Error = await getError(async () =>
+      undiciFetch('http://localhost:5050'),
+    );
+
+    expect(error.cause).toMatchObject({
+      address: '::1',
+      port: 3000,
+    });
+  });
+
+  it('should be able to no proxy using native fetch', async () => {
+    const logger = loggerToWinstonLogger(mockServices.logger.mock());
+
+    // Grab the subscriber function and use mutable config data to mock a config file change
+    // const mutableConfigSource = MutableConfigSource.create({ data: {} });
+    const config = await ConfigSources.toConfig(
+      StaticConfigSource.create({
+        data: {
+          proxy: {
+            http_proxy: 'http://localhost:3000',
+            https_proxy: 'http://localhost:3000',
+            no_proxy: 'localhost:5050',
+          },
+        },
+      }),
+    );
+
+    await createProxyAgent({
+      config,
+      logger,
+    });
+
+    const error: Error = await getError(async () =>
+      undiciFetch('http://localhost:5050'),
+    );
+
+    expect(error.cause).toMatchObject({
+      address: '::1',
+      port: 5050,
+    });
+  });
+
+  it('should be able to proxy using node fetch', async () => {
+    const logger = loggerToWinstonLogger(mockServices.logger.mock());
+
+    // Grab the subscriber function and use mutable config data to mock a config file change
+    const mutableConfigSource = MutableConfigSource.create({ data: {} });
+    const config = await ConfigSources.toConfig(
+      ConfigSources.merge([
+        StaticConfigSource.create({
+          data: {
+            proxy: {
+              http_proxy: 'http://localhost:3000',
+              https_proxy: 'http://localhost:3000',
+            },
+          },
+        }),
+        mutableConfigSource,
+      ]),
+    );
+
+    await createProxyAgent({
+      config,
+      logger,
+    });
+
+    const error: Error = await getError(async () =>
+      nodeFetch('http://localhost:5050'),
+    );
+
+    expect(error.message).toContain('::1:3000');
+  });
+});
--- plugins/http-client-proxy-backend/src/service/proxy.ts
+++ plugins/http-client-proxy-backend/src/service/proxy.ts
@@ -19,10 +19,10 @@ import { Logger } from 'winston';
 import { bootstrap } from 'global-agent';
 import {
   setGlobalDispatcher,
-  EnvHttpProxyAgent,
   getGlobalDispatcher,
   Dispatcher,
   ProxyAgent,
+  Agent,
 } from 'undici';
 
 export interface ProxyOptions {
@@ -30,39 +30,46 @@ export interface ProxyOptions {
   config: Config;
 }
 
+interface ProxyRule {
+  hostname: string;
+  port: number;
+}
+
+const DEFAULT_PORTS: { [key: string]: number } = {
+  http: 80,
+  https: 443,
+};
+
 const getProxyAgent = (proto: string) => {
-  let agent: ProxyAgent | undefined;
+  console.log('getProxyAgent() Proto: ', proto);
+  console.log('getProxyAgent() Http: ', process.env.BACKSTAGE_HTTP_PROXY);
+  console.log('getProxyAgent() Https: ', process.env.BACKSTAGE_HTTPS_PROXY);
 
   if (proto !== 'http' && proto !== 'https') {
-    return agent;
+    return undefined;
   }
 
+  // https is optional
+  // do http over https if it's specified as an option
   if (proto === 'http') {
-    agent = process.env.BACKSTAGE_HTTPS_PROXY
+    return process.env.BACKSTAGE_HTTPS_PROXY
       ? new ProxyAgent(process.env.BACKSTAGE_HTTPS_PROXY)
       : undefined;
   }
 
-  if (!agent) {
-    agent = process.env.BACKSTAGE_HTTP_PROXY
-      ? new ProxyAgent(process.env.BACKSTAGE_HTTP_PROXY)
-      : undefined;
-  }
-  return agent;
+  // proto is https or https isn't specified. get agent for http proxy
+  return process.env.BACKSTAGE_HTTP_PROXY
+    ? new ProxyAgent(process.env.BACKSTAGE_HTTP_PROXY)
+    : undefined;
 };
 
 export async function createProxyAgent(configOptions: ProxyOptions) {
   // Read existing environment variables
+
   const userConfig = {
-    httpProxy: configOptions.config
-      .getOptionalConfig('proxy.http_proxy')
-      ?.get<string>(),
-    httpsProxy: configOptions.config
-      .getOptionalConfig('proxy.https_proxy')
-      ?.get<string>(),
-    noProxy: configOptions.config
-      .getOptionalConfig('proxy.no_proxy')
-      ?.get<string>(),
+    httpProxy: configOptions.config.getOptionalString('proxy.http_proxy'),
+    httpsProxy: configOptions.config.getOptionalString('proxy.https_proxy'),
+    noProxy: configOptions.config.getOptionalString('proxy.no_proxy'),
   };
 
   // Set environment variables based on user config
@@ -82,18 +89,55 @@ export async function createProxyAgent(configOptions: ProxyOptions) {
   // Global agent configures proxy agent used by node fetch requests globally
   bootstrap({ environmentVariableNamespace: 'BACKSTAGE_' });
 
-  // const envHttpProxyAgent = new EnvHttpProxyAgent({
-  //   httpProxy: process.env.BACKSTAGE_HTTP_PROXY,
-  //   httpsProxy: process.env.BACKSTAGE_HTTPS_PROXY,
-  //   noProxy: process.env.BACKSTAGE_NO_PROXY,
-  // });
-
   // Create a default dispatcher
-  const defaultDispatcher = getGlobalDispatcher();
+  const defaultDispatcher = new Agent();
 
-  const noProxyRules = (process.env.BACKSTAGE_NO_PROXY ?? '')
+  const noProxyValue = process.env.BACKSTAGE_NO_PROXY ?? '';
+
+  const noProxyRules: ProxyRule[] = noProxyValue
     .split(',')
-    .map(rule => rule.trim());
+    .map(rule => rule.trim())
+    .filter(rule => rule.length > 0)
+    .map(entry => {
+      const parsed = entry.match(/^(.+):(\d+)$/);
+      return {
+        hostname: (parsed ? parsed[1] : entry).toLowerCase(),
+        port: parsed ? Number.parseInt(parsed[2], 10) : 0,
+      };
+    });
+
+  console.log('No proxy rules: ', noProxyRules);
+
+  function shouldProxy(hostname: string, port: number) {
+    if (noProxyRules.length === 0) {
+      // Always proxy if NO_PROXY is not set or empty.
+      return true;
+    }
+
+    if (noProxyValue === '*') {
+      // Never proxy if wildcard is set.
+      return false;
+    }
+
+    for (const entry of noProxyRules) {
+      if (entry.port !== port) {
+        // Skip if ports don't match
+        continue;
+      }
+
+      if (!/^[.*]/.test(entry.hostname)) {
+        // No wildcards, so don't proxy only if there is not an exact match.
+        if (hostname === entry.hostname) {
+          return false;
+        }
+      } else if (hostname.endsWith(entry.hostname.replace(/^\*/, ''))) {
+        // Don't proxy if the hostname ends with the no_proxy host.
+        return false;
+      }
+    }
+
+    return true;
+  }
 
   setGlobalDispatcher(
     new (class extends Dispatcher {
@@ -102,23 +146,32 @@ export async function createProxyAgent(configOptions: ProxyOptions) {
         handler: Dispatcher.DispatchHandlers,
       ) {
         if (options.origin) {
-          const { host, protocol } =
+          const { host, protocol, port } =
             typeof options.origin === 'string'
               ? new URL(options.origin)
               : options.origin;
-          if (
-            !noProxyRules.some(
-              rule =>
-                rule === '*' ||
-                (rule.startsWith('.') ? host.endsWith(rule) : host === rule),
-            )
-          ) {
-            const proxyAgent = getProxyAgent(protocol);
+
+          // Stripping ports in this way instead of using parsedUrl.hostname to make
+          // sure that the brackets around IPv6 addresses are kept.
+          const nProto = protocol.endsWith(':')
+            ? protocol.slice(0, -1)
+            : protocol;
+
+          const nHost = host.replace(/:\d*$/, '').toLowerCase();
+
+          const nPort =
+            Number.parseInt(port, 10) || DEFAULT_PORTS[protocol] || 0;
+
+          if (shouldProxy(nHost, nPort)) {
+            console.log('proxying!!!');
+            const proxyAgent = getProxyAgent(nProto);
             if (proxyAgent) {
-              proxyAgent.dispatch(options, handler);
+              return proxyAgent.dispatch(options, handler);
             }
           }
+          console.log('not proxying! :(');
         }
+
         return defaultDispatcher.dispatch(options, handler);
       }
     })(),
